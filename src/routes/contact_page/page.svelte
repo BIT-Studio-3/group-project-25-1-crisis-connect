e72f554c543ddcaf4bef8fa4566ce351a3cf1bde
JayDos89 <everjr1@student.op.ac.nz>
2025-02-26 19:41:43 +1300

    Added link to contact page on home page,

--- src/routes/contact_page/page.svelte
+++ src/routes/contact_page/page.svelte
@@ -1,1 +1,11 @@
+<script>
+     import Header from "$lib/Header.svelte";
+     import Footer from "$lib/Footer.svelte";
+     import Layout from "../+layout.svelte";
+</script>
+
+<Header />
+
 <h1>Contact Page!</h1>
+
+<Footer />
