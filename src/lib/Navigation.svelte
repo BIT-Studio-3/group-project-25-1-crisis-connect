f2a96f54fe7d41fd0830f0d0d7c24b4f3300a7e7
Joanna Marowa <marojm1@student.op.ac.nz>
2025-02-28 11:12:51 +1300

    fix(svelte): new nav component to make simplest nav

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<section>
+    <nav>
+        <a href="/weather_reports_page">Weather Reports</a>
+        <a href="/seismic_reports_page">Seismic Activity</a>
+        <a href="/road_conditions_page"> Road Conditions</a>
+        <a href="/volcanic_activity_page">Volcanic Activity</a>
+        <a href="/fire_and_emergency_page" >Fire and Emergency Reports</a>
+        <a href="/rss">RSS Feed</a>
+        <a href="/alert_system" >Post an Alert</a>
+    </nav>
+</section>
