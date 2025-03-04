cb45b499fb42a33097b64dbee3f9dfb0faf02830
JayDos89 <everjr1@student.op.ac.nz>
2025-03-04 13:48:59 +1300

    Fix (contact_page)

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section>
-    <!--This will be a drop down menu that will decend on click or mouse hover (Live Data)-->
+    <!--This will be a drop down menu that will descend on click or mouse hover (Live Data)-->
         <nav><h1>Live Data</h1><br>         
             <li><a href="/weather_reports_page">Weather Reports</a></li>
             <li><a href="/seismic_reports_page">Seismic Activity</a></li>
@@ -11,17 +11,18 @@
             <li><a href="/volcanic_activity_page">Volcanic Activity</a></li>
             <li><a href="/fire_and_emergency_page" >Fire and Emergency Reports</a></li>
             <li><a href="/rss">RSS Feed</a></li>
-            <li><a href="/alert_system" >Post an Alert!!!</a></li>      
+            <li><a href="/alert_system" >Post an Alert!!!</a></li> 
+                 
         </nav>
 </section>
 
 <section>
-    <!--This will be a drop down menu that will decend on click or mouse hover (Contact)-->
+    <!--This will be a drop down menu that will descend on click or mouse hover (Contact)-->
         <nav><h1>Contact</h1><br>
-            <li>Contact services</li>
+            <li><a href="/contact_page">Contact services</a></li>         
             <li>Contact us "the developers"</li>
             <li>Group communication</li>        <!--Just an idea for a feature-->
-            <li>Location based live chatt</li>  <!--Also a possible feature idea-->
+            <li>Location based live chat</li>  <!--Also a possible feature idea-->
         </nav>
 </section>
 
---
+++ src/routes/contact_page/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import Navigation from "$lib/Navigation.svelte";
+     
+</script>
+
+<h1>Contact Page!</h1>
+
