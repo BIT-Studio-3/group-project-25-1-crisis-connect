f21881f0406e8e3d1f8431ccd7c8566fe19fc7c9
JayDos89 <EVERJR1@student.op.ac.nz>
2025-03-04 21:49:47 +1300

    Feat: Contact list information boxes

--- src/routes/contact_page/+page.svelte
+++ src/routes/contact_page/+page.svelte
@@ -2,8 +2,59 @@
     import Header from "$lib/Header.svelte";
     import Footer from "$lib/Footer.svelte";
     import Navigation from "$lib/Navigation.svelte";
-     
+    
 </script>
 
 <h1>Contact Page!</h1>
 
+<div class="container">
+    <h2>Firefighters</h2><br>
+    <ul>
+        <li>Name: Spencer Williams</li>
+        <li>Role: Rescue Fire and Safety Officer</li>
+        <li>Email: </li>
+        <li>Work phone: 03777555</li>
+        <li>Cell phone: "Hidden"</li>
+        <li>Address: "Hidden"</li>
+    </ul><br>
+    <ul>
+        <li>Name: Spencer Williams</li>
+        <li>Role: Rescue Fire and Safety Officer</li>
+        <li>Email: </li>
+        <li>Work phone: 03777555</li>
+        <li>Cell phone: "Hidden"</li>
+        <li>Address: "Hidden"</li>
+    </ul>
+</div>
+
+<div class="container">
+    <h2>Civil defence</h2><br>
+    <ul>
+        <li>Name: Lois Parker</li>
+        <li>Role: Medic</li>
+        <li>Email: </li>
+        <li>Work phone: "Hidden"</li>
+        <li>Cell phone: 025 787 989</li>
+        <li>Address: "Hidden"</li>
+    </ul><br>
+    <ul>
+        <li>Name: Lois Parker</li>
+        <li>Role: Medic</li>
+        <li>Email: </li>
+        <li>Work phone: "Hidden"</li>
+        <li>Cell phone: 025 787 989</li>
+        <li>Address: "Hidden"</li>
+    </ul>
+</div>
+
+<style>
+    .container{
+        border: 2px solid #ddd;
+        border-radius: 8px;
+        padding: 20px;
+        width: 300px;
+        background-color: #f9f9f9;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        margin: 10px;
+    }
+</style>
