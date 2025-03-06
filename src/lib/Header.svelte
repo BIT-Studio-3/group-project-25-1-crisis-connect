1c10e825bf54c3658326ff40d4229e4667dc8810
Mustafa12315 <HABIM3@student.op.ac.nz>
2025-03-07 11:28:19 +1300

    Feat(Js):Added the Header.svelte

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,24 +1,25 @@
 <script>
-    
+  
 </script>
 
 <section>
-    <!--This will be a drop down menu that will decend on click or mouse hover (Live Data)-->
-        <nav><h1>Live Data</h1><br>        
+    <!--This will be a drop down menu that will descend on click or mouse hover (Live Data)-->
+        <nav><h1>Live Data</h1><br>         
             <li><a href="/weather_reports_page">Weather Reports</a></li>
             <li><a href="/seismic_reports_page">Seismic Activity</a></li>
             <li><a href="/road_conditions_page"> Road Conditions</a></li>
             <li><a href="/volcanic_activity_page">Volcanic Activity</a></li>
             <li><a href="/fire_and_emergency_page" >Fire and Emergency Reports</a></li>
             <li><a href="/rss">RSS Feed</a></li>
-            <li><a href="/alert_system" >Post an Alert!!!</a></li>      
+            <li><a href="/alert_system" >Post an Alert!!!</a></li> 
+                 
         </nav>
 </section>
 
 <section>
-    <!--This will be a drop down menu that will decend on click or mouse hover (Contact)-->
+    <!--This will be a drop down menu that will descend on click or mouse hover (Contact)-->
         <nav><h1>Contact</h1><br>
-            <li>Contact services</li>
+            <li><a href="/contact_page">Contact services</a></li>         
             <li>Contact us "the developers"</li>
             <li>Group communication</li>        <!--Just an idea for a feature-->
             <li>Location based live chat</li>  <!--Also a possible feature idea-->
@@ -27,9 +28,14 @@
 
 
 <style>
-    
+    /*.box{
+        display: flex;
+        justify-content: flex-start;
+        gap: 20px;
+        align-items: center;
+    }*/
     a {
-       
+        
         text-decoration: none;
     }
     li{
