b9b8e4fdc0f0eab94a1843b3ce76d8c5974cf65d
JayDos89 <everjr1@student.op.ac.nz>
2025-03-06 18:51:33 +1300

    style: and added content.

--- src/routes/contact_page/+page.svelte
+++ src/routes/contact_page/+page.svelte
@@ -4,23 +4,29 @@
     import Navigation from "$lib/Navigation.svelte";
     
 </script>
+
 <div class="box">
     <h1>Emergency Services</h1><br>
     <h3>Verify your account to become a listee and see "Hidden" contact info</h3>
     <div class="box2">
-        <h4>Click her to register</h4>
+        <h4>Click here to register</h4>
+    </div>
+    <div class="box3">
+        <h4>Download an offline safe copy</h4> 
     </div>
 </div>
 
 <div class="container">
     <h2>Fire and Emergency</h2><br>
     <ul>
+        <li>Region: Otago</li>
         <li>Name: Spencer Williams</li>
         <li>Role: Rescue Fire and Safety Officer</li>
         <li>Email: </li>
         <li>Work phone: 03777555</li>
         <li>Cell phone: "Hidden"</li>
         <li>Address: "Hidden"</li>
+        <li>Radio channel: *****</li>
     </ul><br>
     <ul>
         <li>Name: Spencer Williams</li>
@@ -43,12 +49,14 @@
 <div class="container">
     <h2>Civil defence</h2><br>
     <ul>
+        <li>Region: Cantabury</li>
         <li>Name: Lois Parker</li>
         <li>Role: Team Leader</li>
         <li>Email: </li>
         <li>Work phone: "Hidden"</li>
         <li>Cell phone: 025 787 989</li>
         <li>Address: "Hidden"</li>
+        <li>Radio channel: *****</li>
     </ul><br>
     <ul>
         <li>Name: Lois Parker</li>
@@ -71,12 +79,14 @@
 <div class="container">
     <h2>Saint Johns</h2><br>
     <ul>
+        <li>Region: Southland</li>
         <li>Name: Sally Duns</li>
         <li>Role: Nurse</li>
         <li>Email: Sally.Duns@mailfence.com</li>
         <li>Work phone: "Hidden"</li>
         <li>Cell phone: "Hidden"</li>
         <li>Address: "Hidden"</li>
+        <li>Radio channel: *****</li> 
     </ul><br>
     <ul>
         <li>Name: Sally Duns</li>
@@ -112,6 +122,15 @@
         margin: 15px;
     }
     .box2{
+        border: 2px solid #6c6c6c;
+        border-radius: 8px;
+        padding: 5px;
+        width: 80px;
+        background-color: #f9f9f9;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        margin: 5px;
+    }
+    .box3{
         border: 2px solid #6c6c6c;
         border-radius: 8px;
         padding: 5px;
