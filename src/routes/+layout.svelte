ff1585ea75ba8a2c4d3949fad4a6f3c72be3b237
Joanna Marowa <marojm1@student.op.ac.nz>
2025-03-10 16:10:37 +1300

    refactor(svelte) : renamed file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
     import '../app.css';
-    import Header from '../lib/Header.svelte';
+    import Header from '../lib/Navigation.svelte';
     import Footer from '../lib/Footer.svelte';
 </script>
 
