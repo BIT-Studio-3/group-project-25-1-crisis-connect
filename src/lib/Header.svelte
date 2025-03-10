e11bf520034925b277fd408b39729e3726ea8f83
Joanna Marowa <marojm1@student.op.ac.nz>
2025-03-10 16:40:26 +1300

    feat(svelte) : simple sidebar

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,40 +1,45 @@
 <script>
-  
 </script>
 
 <section>
-    <!--This will be a drop down menu that will descend on click or mouse hover (Live Data)-->
-        <nav><h1>Live Data</h1><br>         
-            <li><a href="/weather_reports_page">Weather Reports</a></li>
-            <li><a href="/seismic_reports_page">Seismic Activity</a></li>
-            <li><a href="/road_conditions_page"> Road Conditions</a></li>
-            <li><a href="/volcanic_activity_page">Volcanic Activity</a></li>
-            <li><a href="/fire_and_emergency_page" >Fire and Emergency Reports</a></li>
-            <li><a href="/rss">RSS Feed</a></li>
-            <li><a href="/alert_system" >Post an Alert!!!</a></li> 
-    
-    <!--This will be a drop down menu that will descend on click or mouse hover (Contact)-->
-       <h1>Contact</h1><br>
-            <li><a href="/contact_page">Contact services</a></li>         
-            <li>Contact us "the developers"</li>
-            <li>Group communication</li>        <!--Just an idea for a feature-->
-            <li>Location based live chat</li>  <!--Also a possible feature idea-->
-        </nav>
+  <nav>
+    <li><a href="/weather_reports_page">Weather Reports</a></li>
+    <li><a href="/seismic_reports_page">Seismic Activity</a></li>
+    <li><a href="/road_conditions_page"> Road Conditions</a></li>
+    <li><a href="/volcanic_activity_page">Volcanic Activity</a></li>
+    <li><a href="/fire_and_emergency_page">Fire and Emergency Reports</a></li>
+    <li><a href="/rss">RSS Feed</a></li>
+    <li><a href="/alert_system">Post an Alert!!!</a></li>
+    <li><a href="/contact_page">Contact services</a></li>
+    <li>Contact us "the developers"</li>
+    <li>Group communication</li>
+    <li>Location based live chat</li>
+  </nav>
 </section>
 
-
 <style>
-    /*.box{
+
+    nav {
+        margin: 0;
+        padding: 0;
+        width : 200px;
+        background-color: #f1f1f1;
+        position: fixed;
+        height : 100%;
+        overflow: auto;
+
+
+    }
+  /*.box{
         display: flex;
         justify-content: flex-start;
         gap: 20px;
         align-items: center;
-    }*/
-    a {
-        
-        text-decoration: none;
     }
-    li{
-        list-style: none;
-    }
+  a {
+    text-decoration: none;
+  }
+  li {
+    list-style: none;
+  } */
 </style>
