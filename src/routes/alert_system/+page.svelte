87138a75bb97bb0186bb88383d2f71b7d3566d5c
Mustafa12315 <HABIM3@student.op.ac.nz>
2025-03-13 17:21:42 +1300

    feat(css):Added section tag to the alert_system page

--- src/routes/alert_system/+page.svelte
+++ src/routes/alert_system/+page.svelte
@@ -1,62 +1,60 @@
-<h1>Alerts</h1>
-<p>
-  This where users will be able to post alerts. They will be able to choose
-  emergency type, level, and region.
-</p>
-<p>
-  Alerts will show up in a feed on another page. This form currently does
-  nothing. Once we learn more about logins, this page will be functional.
-</p>
+<section>
+  <h1>Alerts</h1>
+  <p>
+    This where users will be able to post alerts. They will be able to choose
+    emergency type, level, and region.
+  </p>
+  <p>
+    Alerts will show up in a feed on another page. This form currently does
+    nothing. Once we learn more about logins, this page will be functional.
+  </p>
 
-<form>
-  <h2>Post Alert:</h2>
-  <div class="container">
-    <label for="title">Title:</label>
+  <form>
+    <h2>Post Alert:</h2>
+    <div class="container">
+      <label for="title">Title:</label>
 
-    <input typ="text" id="title" name="alert-title" />
+      <input typ="text" id="title" name="alert-title" />
 
-    <label for="type">Emergency type:</label>
+      <label for="type">Emergency type:</label>
 
-    <select for="type" id="type">
-      <option value="">Please select an emergency type</option>
-      <option value="fire">Fire</option>
-      <option value="flood">Flooding</option>
-      <option value="quake">Earthquake damage</option>
-    </select>
+      <select for="type" id="type">
+        <option value="">Please select an emergency type</option>
+        <option value="fire">Fire</option>
+        <option value="flood">Flooding</option>
+        <option value="quake">Earthquake damage</option>
+      </select>
 
-    <label for="level">Select alert level:</label>
+      <label for="level">Select alert level:</label>
 
-    <select name="level" id="level">
-      <option value="one">1</option>
-      <option value="two">2</option>
-      <option value="three">3</option>
-      <option value="four">4</option>
-      <option value="five">5</option>
-    </select>
+      <select name="level" id="level">
+        <option value="one">1</option>
+        <option value="two">2</option>
+        <option value="three">3</option>
+        <option value="four">4</option>
+        <option value="five">5</option>
+      </select>
 
-    <label for="region">Choose Region:</label>
+      <label for="region">Choose Region:</label>
 
-    <select name="region" id="region">
-      <option value="">Please select a region</option>
-      <option value="otago">Otago</option>
-      <option value="canterbury">Canterbury</option>
-      <option value="southland">Southland</option>
-    </select>
+      <select name="region" id="region">
+        <option value="">Please select a region</option>
+        <option value="otago">Otago</option>
+        <option value="canterbury">Canterbury</option>
+        <option value="southland">Southland</option>
+      </select>
 
-    <label for="description">Description:</label>
+      <label for="description">Description:</label>
 
-    <textarea id="description" name="description"></textarea>
-  </div>
-  <div>
-    <button type="submit">Submit</button>
-  </div>
-</form>
+      <textarea id="description" name="description"></textarea>
+    </div>
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+</section>
 
 <style>
-  * {
-    font-family: sans-serif;
-    color: #333;
-  }
   textarea {
     vertical-align: top;
     height: 10em;
@@ -83,11 +81,11 @@
     max-width: fit-content;
     margin: 0.5em;
   }
-  select{
+  select {
     max-width: fit-content;
     margin: 0.5em;
   }
-  textarea{
+  textarea {
     margin: 0.5em;
   }
   @media (max-width: 1281px) {
@@ -98,7 +96,7 @@
       width: auto;
     }
     label {
-      align-self:center;
+      align-self: center;
     }
   }
 </style>
