b44dd4424f553489fbdb644ba7a7b78acc064c87
JayDos89 <everjr1@student.op.ac.nz>
2025-03-25 19:01:58 +1300

    Created the Hazard form with basic layout and temporary page link

---
+++ src/routes/hazards/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<section>
+
+<h2>Post local hazards here</h2>
+
+<p>Let us know below if you come accross any hazards during distasters to help avoid accidents</p>
+
+<div class="container">
+    <form>
+
+
+    </form>
+</div>
+
+
+</section>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height:  50em;
+    }
+</style>
+
+
