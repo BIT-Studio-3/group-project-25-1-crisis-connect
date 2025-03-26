0f4fe2b6b2831c789aada0cdae683e0bdbea2e38
JayDos89 <everjr1@student.op.ac.nz>
2025-03-26 21:39:13 +1300

    Feat: Added input boxes for the hazard form

--- src/routes/hazards/+page.svelte
+++ src/routes/hazards/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-
+    let address = "";
+    let area = "";
+    let hazard = "";
 </script>
 
 <section>
@@ -10,11 +12,25 @@
 
 <div class="container">
     <form>
-
-
+        <div>
+            <label for="address">Address</label>
+            <input type="text" id="address" bind:value={address}>
+        </div><br>
+        <div>
+            <label for="address">Area</label>
+            <input type="text" id="area" bind:value={area}>
+        </div><br>
+        <div>
+            <label for="hazard">Hazard</label>
+            <input type="text" id="hazard" bind:value={hazard}>
+        </div><br>
+        <button>Submit</button>
     </form>
 </div>
 
+<p>Address: {address}</p>
+<p>Area: {area}</p>
+<p>Hazard: {hazard}</p>
 
 </section>
 
@@ -23,7 +39,11 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        height:  50em;
+        height:  20em;
+    }
+    input {
+        font-size: small;
+        color: rgb(0, 0, 0);
     }
 </style>
 
