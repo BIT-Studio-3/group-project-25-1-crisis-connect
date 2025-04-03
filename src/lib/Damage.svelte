e21c666978bd6627ebedd4d61d028c3a6b56fa25
Joanna Marowa <marojm1@student.op.ac.nz>
2025-04-04 08:37:01 +1300

    feat(js) : add damage component

--- src/lib/Damage.svelte
+++ src/lib/Damage.svelte
@@ -1,21 +1,41 @@
-<!--component that will showcase damage data-->
+
 <script>
-export let address;
-export let area;
+export let id;
+export let streetNumber;
+export let streetName;
+export let city;
+export let region;
 export let type;
 export let description;
 </script>
 
 <section>
-    <article>
-        <h2>{area}</h2>
-        <h3>{address}</h3>
-        <h3>{type}</h3>
-        <p>{description}</p>
-    </article>
+    <article >
+        <header>
+          <h1>Property Details</h1>
+          <p class="property-id">ID: {id}</p>
+        </header>
+      
+        <section class="address">
+          <h2>Address :</h2>
+          <p>{streetNumber} {streetName}, {city}, {region}</p>
+        </section>
+      
+        <section class="type">
+          <h2>Type :</h2>
+          <p>{type}</p>
+        </section>
+      
+        <section class="description">
+          <h2>Description</h2>
+          <p>{description}</p>
+        </section>
 </section>
 
-
+<h2>{area}</h2>
+<h3>{address}</h3>
+<h3>{type}</h3>
+<p>{description}</p>
 <style>
 
 </style>
