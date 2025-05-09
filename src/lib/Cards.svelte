0bc7941b638d692076ce6e64558619a095fb1a52
JayDos89 <everjr1@student.op.ac.nz>
2025-05-09 15:37:29 +1200

    Feat: Created a basic Task Card component layout and implemented into the Tasks Card page.

---
+++ src/lib/Cards.svelte
@@ -0,0 +1,41 @@
+<script>
+    
+</script>
+
+<div class="cardcontainer">
+    <ul>
+        <h3>Title</h3>
+        <p>Date: 34/19/01</p>
+        <p>Name: Jimmy</p>    
+    </ul>
+    <ul>
+        <h3>Title</h3>
+        <p>Date: 34/19/01</p>
+        <p>Name: Jimmy</p>    
+    </ul>
+    <ul>
+        <h3>Title</h3>
+        <p>Date: 34/19/01</p>
+        <p>Name: Jimmy</p>    
+    </ul>
+    <ul>
+        <h3>Title</h3>
+        <p>Date: 34/19/01</p>
+        <p>Name: Jimmy</p>    
+    </ul>
+</div>
+
+<style>
+
+.cardcontainer {
+    display: grid;
+    grid-template-columns: repeat(2, 325px);
+    grid-template-rows: auto;
+    gap: 1em;
+    margin-top: 1em;
+    margin-bottom: 1em;
+    margin-left: 2em;
+    margin-right: 1em;
+}
+ 
+</style>
